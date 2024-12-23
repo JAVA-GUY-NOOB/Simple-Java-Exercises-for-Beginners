41a82b8c38a276ce9ccafb1459274c3e2ef64227
GUY-NOOB <gideonmurimi01@gmail.com>
2024-12-23 04:49:16 +0300

    confused

--- src/simple-tasks/Task31.java
+++ src/simple-tasks/Task31.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
  */
 public class Task31 {
 
-    public static void main(final String[] args) {
+    public  static void main(final String[] args) {
 int[] a = {3, 2, 5, 4, 1};
         System.out.println("Original Array: " + Arrays.toString(a));
         Arrays.sort(a);
         System.out.println("Sorted Array: " + Arrays.toString(a));
     }
-}
+} 
